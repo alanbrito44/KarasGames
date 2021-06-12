0b21769f705374a51d44964c60e58565e55ba1f1
alanbrito44 <castillo.alan1995@gmail.com>
2021-06-11 18:19:57 -0600

    Commit seccion paginas relacionadas

--- Template/_blogs.php
+++ Template/_blogs.php
@@ -1,31 +1,31 @@
 <!--Blogs-->
     <section id="blogs">
         <div class="container py-4">
-            <h4 class="font-baskerville font-size-20">Blogs sobre video juegos</h4>
+            <h4 class="font-baskerville font-size-20">Paginas relacionadas a la compra de videojuegos</h4>
             <hr>
             <div class="owl-carousel owl-theme">
                 <div class="item">
-                    <div class="card border-0 me-5" style="width: 18rem;">
-                        <h5 class="card-title font-size-16">Blogs con relacion a Games</h5>
-                        <img src="assets/card1.jpg" alt="cart image" class="card-img-top">
-                        <p class="card-text font-size-14 text-black-50 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, nemo! Quia cum error consequuntur molestiae eaque soluta, quasi vitae numquam!</p>
-                        <a href="#" class="color-second text-left btn">Go somewhere</a>
+                    <div class="card border-0 me-5" style="width: 25rem;">
+                        <h5 class="card-title font-size-16">EPIC Games</h5>
+                        <img src="./assets/Tiendas relacionadas/epic.png" alt="cart image" class="card-img-top" style="height: 250px;">
+                        <p class="card-text font-size-14 text-black-50 py-1">es una empresa de desarrollo de videojuegos estadounidense con sede en Cary (Carolina del Norte), ahora asociada con la compañía china Tencent Holdings.1​ Ellos son principalmente conocidos por su tecnología Unreal Engine, que ha impulsado su popular serie de juegos de disparos en primera persona Unreal y la saga Gears of War, así como el mundialmente famoso Fortnite.</p>
+                        <a href="https://www.epicgames.com/store/es-ES/" target="_blank" class="btn btn-warning">Visitar</a>
                     </div>
                 </div>
                 <div class="item">
-                    <div class="card border-0 me-5" style="width: 18rem;">
-                        <h5 class="card-title font-size-16">Blogs con relacion a Games</h5>
-                        <img src="assets/card2.jpg" alt="cart image" class="card-img-top">
-                        <p class="card-text font-size-14 text-black-50 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, nemo! Quia cum error consequuntur molestiae eaque soluta, quasi vitae numquam!</p>
-                        <a href="#" class="color-second text-left btn">Go somewhere</a>
+                    <div class="card border-0 me-5" style="width: 25rem;">
+                        <h5 class="card-title font-size-16">STEAM Store</h5>
+                        <img src="./assets/Tiendas relacionadas/steam.jpg" alt="cart image" class="card-img-top" style="height: 250px;">
+                        <p class="card-text font-size-14 text-black-50 py-1">es una plataforma de distribución digital de videojuegos desarrollada por Valve Corporation. Fue lanzada en septiembre de 2003 como una forma para Valve de proveer actualizaciones automáticas a sus juegos, pero finalmente se amplió para incluir juegos que no son de Valve sino de terceros. Steam ofrece protección contra piratería4​5​, servidores de emparejamiento, transmisiones de vídeo y servicios de redes sociales.</p>
+                        <a href="https://store.steampowered.com/" target="_blank" class="btn btn-warning">Visitar</a>
                     </div>
                 </div>
                 <div class="item">
-                    <div class="card border-0 me-5" style="width: 18rem;">
-                        <h5 class="card-title font-size-16">Blogs con relacion a Games</h5>
-                        <img src="assets/card3.jpg" alt="cart image" class="card-img-top">
-                        <p class="card-text font-size-14 text-black-50 py-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, nemo! Quia cum error consequuntur molestiae eaque soluta, quasi vitae numquam!</p>
-                        <a href="#" class="color-second text-left btn">Go somewhere</a>
+                    <div class="card border-0 me-5" style="width: 25rem;">
+                        <h5 class="card-title font-size-16">G2A</h5>
+                        <img src="./assets/Tiendas relacionadas/g2a.jpg" alt="cart image" class="card-img-top" style="height: 250px;">
+                        <p class="card-text font-size-14 text-black-50 py-1">Marketplace digital global especializado en productos gaming. Sus oficinas centrales tienen sede en Hong Kong, pero dispone de oficinas en varios países incluyendo Polonia, Países Bajos y China. El sitio afirma tener más 260.000 vendedores, 50.000 productos digitales y 700 empleados en total. El producto principal ofrecido en G2A son las claves de activación de juegos digitales para plataformas como Steam, Origin y Xbox.</p>
+                        <a href="https://www.g2a.com/" target="_blank" class="btn btn-warning">Visitar</a>
                     </div>
                 </div>
             </div>
