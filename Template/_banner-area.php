b7c48c94dd755dfbf0f8e5d5dbc4a606d324e04f
alanbrito44 <castillo.alan1995@gmail.com>
2021-06-13 03:12:29 -0600

    Commit imagenes slider

--- Template/_banner-area.php
+++ Template/_banner-area.php
@@ -9,43 +9,37 @@
             </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="./assets/Slilder1.jpg" class="d-block w-100" alt="...">
+                        <img src="./assets/slider1.jpg" class="d-block w-100" alt="...">
                         <div class="carousel-caption d-none d-md-block">
                             <div class="row">
                                 <div class="col-12 col-lg-6 mb-5 mb-lg-0">
-                                    <h5 class="display-5">Probando tema</h5>
+                                    <h5 class="display-5">Assassin's Creed: Valhalla</h5>
                                     <hr>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur non veniam exercitationem est similique? Quis commodi sed et dignissimos necessitatibus.</p>
-                                    <p class="fs-3">$50.00</p>
-                                    <a class="btn btn-primary btn-lg" href="#" role="button">Comprar ahora</a>
+                                    <p>Assassin's Creed Valhalla es un videojuego desarrollado por Ubisoft Montreal y publicado por Ubisoft. Es el decimosegundo en importancia y el vigesimosegundo lanzado dentro de la saga de Assassin Creed, y sucesor al juego del 2018 Assassin's Creed Odyssey.</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="./assets/Slilder2.jpg" class="d-block w-100" alt="...">
+                        <img src="./assets/slider2.jpg" class="d-block w-100" alt="...">
                         <div class="carousel-caption d-none d-md-block">
                             <div class="row">
                                 <div class="col-12 col-lg-6 mb-5 mb-lg-0">
-                                    <h5 class="display-5">Probando tema</h5>
+                                    <h5 class="display-5">The Elder Scrolls V: Skyrim</h5>
                                     <hr>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur non veniam exercitationem est similique? Quis commodi sed et dignissimos necessitatibus.</p>
-                                    <p class="fs-3">$50.00</p>
-                                    <a class="btn btn-primary btn-lg" href="#" role="button">Comprar ahora</a>
+                                    <p>The Elder Scrolls V: Skyrim es un videojuego de rol de acción del tipo mundo abierto desarrollado por Bethesda Game Studios y publicado por Bethesda Softworks. Skyrim es la quinta entrega de la saga de videojuegos de acción y fantasía de la serie The Elder Scrolls y es posterior a The Elder Scrolls IV: Oblivion y predecesor de The Elder Scrolls Online. </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="./assets/Slilder3.jpg" class="d-block w-100" alt="...">
+                        <img src="./assets/slider3.jpg" class="d-block w-100" alt="...">
                         <div class="carousel-caption d-none d-md-block">
                             <div class="row">
                                 <div class="col-12 col-lg-6 mb-5 mb-lg-0">
-                                    <h5 class="display-5">Probando tema</h5>
+                                    <h5 class="display-5">Anthem</h5>
                                     <hr>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur non veniam exercitationem est similique? Quis commodi sed et dignissimos necessitatibus.</p>
-                                    <p class="fs-3">$50.00</p>
-                                    <a class="btn btn-primary btn-lg" href="#" role="button">Comprar ahora</a>
+                                    <p>Anthem es un videojuego de acción multijugador en línea desarrollado por Bioware, creadores de las sagas Mass Effect y Dragon Age, y distribuido por Electronic Arts, que utiliza el motor gráfico Frostbite. Su fecha de lanzamiento fue el 22 de febrero de 2019 en las plataformas PlayStation 4, Xbox One y Microsoft Windows.1​</p>
                                 </div>
                             </div>
                         </div>
