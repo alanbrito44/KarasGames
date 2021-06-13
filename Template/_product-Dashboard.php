d1171fc74d12df26c663e34989666a3e49723e87
alanbrito44 <castillo.alan1995@gmail.com>
2021-06-12 19:04:00 -0600

    Commit dashboard productos

---
+++ Template/_product-Dashboard.php
@@ -0,0 +1,208 @@
+<?php
+if($_POST){
+    //insetar juego
+    if(isset($_POST["btnGuardar"])){
+
+        //subir imagen a la base de datos
+        $nombreimg = $_FILES["FileImagen"]["name"];
+        $archivo = $_FILES["FileImagen"]["tmp_name"];
+        $ruta = "./assets/catalogo";
+        $ruta = $ruta."/".$nombreimg;
+        move_uploaded_file($archivo,$ruta);
+        //subir imagen a la base de datos
+
+		$distribuidora = $_REQUEST["txtDistribuidora"];
+        $titulo = $_REQUEST["txtTitulo"];
+        $precio = $_REQUEST["txtPrecio"];
+        $ruta;//se pone la ruta del archivo subido
+		$fecha = $_REQUEST["txtFecha"];
+        $categoria = $_REQUEST["cmbCodCat"];
+		$descripcion = $_REQUEST["txtDescripcion"];
+		$codigo = $_REQUEST["txtCodigo"];
+		
+		$dash->insertar($distribuidora,$titulo,$precio,$ruta,$fecha,$categoria,$descripcion,$codigo);
+    }
+    //insetar juego
+
+	//modificar juego
+    if(isset($_POST["btnModificar"])){
+
+        //subir imagen a la base de datos
+        $nombreimg = $_FILES["FileImagen"]["name"];
+        $archivo = $_FILES["FileImagen"]["tmp_name"];
+        $ruta = "./assets/catalogo";
+        $ruta = $ruta."/".$nombreimg;
+        move_uploaded_file($archivo,$ruta);
+        //subir imagen a la base de datos
+
+		$distribuidora = $_REQUEST["txtDistribuidora"];
+        $titulo = $_REQUEST["txtTitulo"];
+        $precio = $_REQUEST["txtPrecio"];
+        $ruta;//se pone la ruta del archivo subido
+		$fecha = $_REQUEST["txtFecha"];
+        $categoria = $_REQUEST["cmbCodCat"];
+		$descripcion = $_REQUEST["txtDescripcion"];
+		$codigo = $_REQUEST["txtCodigo"];
+		
+		if($_REQUEST["modificacion"]=="si"){
+            $dash->modificar($distribuidora,$titulo,$precio,$ruta,$fecha,$categoria,$descripcion,$codigo);
+            header("location:./dashboardPro.php");
+        }else{
+            header("location:./dashboardPro.php");
+        }	
+    }
+    //modificar juego
+
+    //eliminar juego
+    if(isset($_POST["btnEliminar"])){
+
+		$codigo = $_REQUEST["txtCodigo"];
+		
+		if($_REQUEST["eliminacion"]=="si"){
+            $dash->eliminar($codigo);
+            header("location:./dashboardPro.php");
+        }else{
+            header("location:./dashboardPro.php");
+        }	
+    }
+    //eliminar juego
+
+}
+?>
+<script type="text/javascript">
+
+	//modificar juego
+	function cargarMo(distribuidora, titulo, precio, imagen, fecha, catagegoria, descripcion,codigo){
+			catagegoria--;
+            document.getElementById("txtDistribuidora").value=distribuidora;
+            document.getElementById("txtTitulo").value=titulo;
+            document.getElementById("txtPrecio").value=precio;
+            document.getElementById("FileImagen").selectedIndex=imagen;
+			document.getElementById("txtFecha").value=fecha;
+            document.getElementById("cmbCodCat").selectedIndex=catagegoria;
+            document.getElementById("txtDescripcion").value=descripcion;
+			document.getElementById("txtCodigo").value=codigo;
+            
+            $("#btnEliminar").hide();
+            $("#btnModificar").show();
+            $("#btnGuardar").hide();
+    }
+        function confirmarMo(){
+            if(confirm("¿Estas seguro de querer modificar el registro?")){
+                document.getElementById("modificacion").value="si";
+            }else{
+                document.getElementById("modificacion").value="no";
+            }
+        }
+        //modificar juego
+
+        function cargarDel(distribuidora, titulo, precio, imagen, fecha, catagegoria, descripcion,codigo){
+			catagegoria--;
+            document.getElementById("txtDistribuidora").value=distribuidora;
+            document.getElementById("txtTitulo").value=titulo;
+            document.getElementById("txtPrecio").value=precio;
+            document.getElementById("FileImagen").selectedIndex=imagen;
+			document.getElementById("txtFecha").value=fecha;
+            document.getElementById("cmbCodCat").selectedIndex=catagegoria;
+            document.getElementById("txtDescripcion").value=descripcion;
+			document.getElementById("txtCodigo").value=codigo;
+            
+            $("#btnEliminar").show();
+            $("#btnModificar").hide();
+            $("#btnGuardar").hide();
+    }
+
+    function confirmarDel(){
+            if(confirm("¿Estas seguro de querer Eliminar el registro?")){
+                document.getElementById("eliminacion").value="si";
+            }else{
+                document.getElementById("eliminacion").value="no";
+            }
+        }
+
+        //nuevo juego
+        $(document).ready(function(){
+            $("#btnNuevo").on("click", function(){
+                $('input[type="text"]').val('');
+                $("#btnEliminar").hide();
+                $("#btnModificar").hide();
+                $("#btnGuardar").show();
+            });
+        });
+        //nuevo juego
+
+</script>
+
+<button type="button" id="btnNuevo" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    Nuevo juego
+</button>
+
+<br>
+
+<section>
+	<div class="container-fluid">
+		<?php
+            echo $dash->tablaProducto();
+        ?>
+	</div>
+</section>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <form action="dashboardPro.php" method="post" enctype="multipart/form-data" id="formuarlio">
+
+                <label class="label label-danger">Codigo juego</label><br>
+                <input type="text" name="txtCodigo" id="txtCodigo" class="form-control" required><br>
+
+                <label class="label label-danger">Distribuidora</label><br>
+                <input type="text" name="txtDistribuidora" id="txtDistribuidora" class="form-control" required><br>
+
+                <label class="label label-danger">Titulo</label><br>
+                <input type="text" name="txtTitulo" id="txtTitulo" class="form-control" required><br>
+
+                <label class="label label-danger">Precio</label><br>
+                <input type="text" name="txtPrecio" id="txtPrecio" class="form-control" required><br>
+
+                <label class="label label-danger">Imagen</label><br>
+                <input type="file" name="FileImagen" id="FileImagen"><br><br>
+
+				<label class="label label-danger">Fecha de transaccion</label><br>
+				<input class="form-control" type="date" id="txtFecha" name="txtFecha">
+
+                <label class="label label-danger">Categoria</label><br>
+                <select name="cmbCodCat" id="cmbCodCat" class="form-control">
+                    <?php
+                        $data = $dash->getComboCategorias();
+                        foreach ($data as $indice=>$valor){
+                            echo ' <option value="'. $indice. '">'. $valor . ' </option>';
+                        }
+                    ?>
+                </select>
+
+				<label class="label label-danger">Descripcion</label><br>
+                <textarea name="txtDescripcion" id="txtDescripcion" class="form-control" required></textarea>
+
+				
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <input type="submit" value="Guardar" name="btnGuardar" id="btnGuardar" class="btn btn-success">
+                    <input type="submit" onclick="confirmarDel()" value="Eliminar" name="btnEliminar" id="btnEliminar" class="btn btn-danger">
+                    <input type="submit" onclick="confirmarMo()" value="Modificar" name="btnModificar" id="btnModificar" class="btn btn-info">
+                </div>
+
+                <!--dos hiden para verificar si se va eliminar o modificar -->
+                <input type="hidden" id="eliminacion" name="eliminacion" value="">
+                <input type="hidden" id="modificacion" name="modificacion" value="">
+            </form>
+        </div>
+        </div>
+    </div>
+</div>
---
+++ database/Dashboard.php
@@ -0,0 +1,91 @@
+<?php
+
+class Dashboard{
+
+    public $db = null;
+
+    public function __construct(DBController $db)
+    {
+        if(!isset($db->con)) return null;
+        $this->db = $db;
+    }
+
+    public function tablaProducto(){
+        $query = "select * from productos";
+        $result = $this->db->con->query($query);
+        $tabla = "
+        <table class='table table-dark table-striped text-center'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>CODIGO</th>
+                    <th>Distribuidora</th>
+                    <th>Titulo</th>
+                    <th>Precio</th>
+                    <th>Imagen</th>
+                    <th>Fecha de registro</th>
+                    <th>Codigo caterogira</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>";
+        while ($fila = mysqli_fetch_assoc($result)){
+            $tabla .= "<tr>";
+            $tabla .= "<td>".$fila["juego_id"]."</td>";
+            $tabla .= "<td>".$fila["juego_cod"]."</td>";
+            $tabla .= "<td>".$fila["juego_distribuidora"]."</td>";
+            $tabla .= "<td>".$fila["juego_nombre"]."</td>";
+            $tabla .= "<td>".$fila["juego_precio"]."</td>";
+            $tabla .= "<td><img src='".$fila["juego_imagen"]."' style='width: 50px; height: 50px;'></td>";
+            $tabla .= "<td>".$fila["juego_registro"]."</td>";
+            $tabla .= "<td>".$fila["categoria_id"]."</td>";
+            $tabla .= "<td><a data-bs-toggle=\"modal\" data-bs-target=\"#staticBackdrop\" class='btn btn-primary'  onclick=\"javascript:cargarMo('".$fila["juego_distribuidora"]."','".$fila["juego_nombre"]."','".$fila["juego_precio"]."','".$fila["juego_imagen"]."','".$fila["juego_registro"]."','".$fila["categoria_id"]."','".$fila["juego_descripcion"]."','".$fila["juego_cod"]."')\">Modificar</a> / ";
+            $tabla .= "<a data-bs-toggle=\"modal\" data-bs-target=\"#staticBackdrop\" class='btn btn-primary'  onclick=\"javascript:cargarDel('".$fila["juego_distribuidora"]."','".$fila["juego_nombre"]."','".$fila["juego_precio"]."','".$fila["juego_imagen"]."','".$fila["juego_registro"]."','".$fila["categoria_id"]."','".$fila["juego_descripcion"]."','".$fila["juego_cod"]."')\">Eliminar</a></td>";  
+            $tabla .= "</tr>";
+        }
+        $tabla .= "
+            </tbody>
+        </table>";
+        return $tabla;
+    }
+
+    public function getComboCategorias(){
+        $query = "select categoria_id, categoria_nombre from categorias";
+        $result = $this->db->con->query($query);
+        $data = array();
+        while($fila = mysqli_fetch_assoc($result)){
+            $data[$fila["categoria_id"]]=$fila["categoria_nombre"];
+        }
+        return $data;
+    }
+
+    public function insertar($distribuidora,$titulo,$precio,$imagen,$fecha,$codigoCat,$descripcion,$codigoJue){
+        $query ="insert into productos values('NULL','$distribuidora','$titulo','$precio','$imagen','$fecha','$codigoCat','$descripcion','$codigoJue')";
+        if($this->db->con->query($query)){
+            echo "<script>alert('Ingresado con exito');</script>";
+        }else{
+            echo "<script>alert('Algo salio mal....');</script>";
+        }
+    }
+
+    public function modificar($distribuidora,$titulo,$precio,$imagen,$fecha,$codigo,$descripcion,$codigoJue){
+        $query ="update productos set juego_distribuidora = '$distribuidora', juego_nombre = '$titulo', juego_precio = '$precio', juego_imagen = '$imagen', juego_registro = '$fecha', categoria_id = '$codigo', juego_descripcion = '$descripcion' where juego_cod = '$codigoJue)' ";
+        if($this->db->con->query($query)){
+            echo "<script>alert('Cambios realizados con exito');</script>";
+        }else{
+            echo "<script>alert('Algo salio mal....');</script>";
+        }
+    }
+
+    public function eliminar($codigoJue){
+        $query ="delete from productos where juego_cod = '$codigoJue)' ";
+        if($this->db->con->query($query)){
+            echo "<script>alert('Eliminacion de juego realizada con exito');</script>";
+        }else{
+            echo "<script>alert('Algo salio mal....');</script>";
+        }
+    }
+
+}
+
+?>
